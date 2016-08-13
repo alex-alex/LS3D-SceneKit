3e6b4db05e7bac3d6acde9ce83ee561d799274bf
Alex Studnička <astudnicka@gmail.com>
2016-08-13 21:45:56 +0200

    Initial Commit

---
+++ Mafia/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Mafia
+//
+//  Created by Alex Studnicka on 8/13/16.
+//  Copyright (c) 2016 Alex Studnicka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
