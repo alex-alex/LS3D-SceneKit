5b803830f5a4058929fd3e3d53c32c0cdcefac81
Alex Studnička <astudnicka@gmail.com>
2018-01-14 01:05:56 +0100

    Remove unused controls

--- Mafia/Game/Control.swift
+++ Mafia/Game/Control.swift
@@ -10,50 +10,31 @@ import Foundation
 
 enum Control: String {
 	case ACTION
-	case DRVLEFT
-	case HOLSTER
-	case RIGHT1
-	case ACTION1
-	case DRVLEFT1
+//	case DRVLEFT
+//	case HOLSTER
 	case HORN
-	case RUN
-	case CLUTCH
-	case DRVRIGHT
-	case HORN1
-	case RUN1
-	case CLUTCH1
-	case DRVRIGHT1
+//	case RUN
+//	case CLUTCH
+//	case DRVRIGHT
 	case INVENTORY
 	case RUNSWITCH
 	case CROUCH
 	case FIRE
 	case JUMP
 	case RELOAD
-	case CROUCH1
-	case FIRE1
-	case JUMP1
 	case SNIPERMODE
-	case DOWN
-	case GEARUP
-	case LEFT
+//	case DOWN
+//	case GEARUP
+//	case LEFT
 	case SPEEDLIMIT
-	case DOWN1
-	case GEARUP1
-	case LEFT1
-	case SPEEDLIMIT1
-	case DRVUP
-	case GEARDOWN
+//	case DRVUP
+//	case GEARDOWN
 	case MAP
 	case UP
-	case DRVUP1
-	case GEARDOWN1
-	case MOTORSWITCH
-	case UP1
-	case DRVDOWN
+//	case MOTORSWITCH
+//	case DRVDOWN
 	case HANDBRAKE
 	case OBJECTIVES
 	case WEAPONDROP
-	case DRVDOWN1
-	case HANDBRAKE1
-	case RIGHT
+//	case RIGHT
 }
